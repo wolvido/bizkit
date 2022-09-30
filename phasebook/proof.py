69d757d30ea95af418811a000754be6d76223fe1
Winzyl <prowinzil@gmail.com>
2022-09-30 21:05:44 +0800

    implemented search with bonus

--- phasebook/proof.py
+++ phasebook/proof.py
@@ -25,30 +25,82 @@ def is_match(arr1,arr2):
         if counter == (len(arr1)):
             return True
 
-
 print(is_match(arr1,arr2))
 
+from data.search_data import USERS
 
-# def is_match(arr2,arr1):
-#     arr1Sort = sorted(arr1)
-#     arr2Sort = sorted(arr2)
-#     shiftCounter = 0
-#     counter = 0
+def search_users(args):
+    output = []
+    if "id" in args:
+        for x in USERS:
+            if x["id"] == args["id"]:
+                output.append(x)
+    if "name" in args:
+        for x in USERS:
+            if args["name"].lower() in x["name"].lower():
+                output.append(x)
+    if "age" in args:
+        for x in USERS:
+            if (x["age"] == args["age"] or x["age"] == args["age"]-1 or x["age"] == args["age"]+1) and x not in output:
+                output.append(x)
+    if "occupation" in args:
+        for x in USERS:
+            if  args["occupation"].lower() in x["occupation"].lower():
+                output.append(x)
+    return output
 
-#     if len(arr1) > len(arr2):
-#         return False
-#     else:
-#         for i in arr1Sort:
-#             while True:
-#                 if shiftCounter > len(arr2) and i != arr2Sort[shiftCounter]:
-#                     return False
-#                 elif i < arr2Sort[shiftCounter]:
-#                     return False                
-#                 elif i != arr2Sort[shiftCounter] and i > arr2Sort[shiftCounter]:
-#                     shiftCounter += 1
-#                 else:
-#                     shiftCounter += 1
-#                     counter += 1
-#                     break
-#             if counter == (len(arr1)+1):
-#                 return True
+print(search_users({
+    "id":"5",
+    "name":"Joe",
+    "age": 30,
+    "occupation":"Arc"
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dragon = {
+#   "id": "2",
+#   "name": "John"
+# }
+
+# if "name" in dragon:
+#     print(dragon["id"])
+
+# test = [3]
+# test2 = iter(test)
+
+# print(next(test2))
+
+# text = [1,3,'a','e']
+## numbers = [x for x in text if x.isdigit()]
+
+# output.append(next((x for x in USERS if x["age"] == args["age"]),None))
+
+# age = []
+# for x in USERS:
+#     if x["age"] == args["age"]:
+#         age.append(x)
+
+
+# x for x in USERS if x["age"] == args["age"]
+
+
+
+# numbers = []
+# for x in text:
+#     if isinstance(x, int):
+#         numbers.append(x)
+
+# print(numbers)
--- phasebook/search.py
+++ phasebook/search.py
@@ -2,14 +2,11 @@ from flask import Blueprint, request
 
 from .data.search_data import USERS
 
-
 bp = Blueprint("search", __name__, url_prefix="/search")
-
 
 @bp.route("")
 def search():
     return search_users(request.args.to_dict()), 200
-
 
 def search_users(args):
     """Search users database
@@ -24,7 +21,23 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
+    output = []
+    if "id" in args:
+        for x in USERS:
+            if x["id"] == args["id"]:
+                output.append(x)
+    if "name" in args:
+        for x in USERS:
+            if args["name"].lower() in x["name"].lower():
+                output.append(x)
+    if "age" in args:
+        for x in USERS:
+            if (x["age"] == int(args["age"]) or x["age"] == int(args["age"])-1 or x["age"] == int(args["age"])+1) and x not in output:
+                output.append(x)
+    if "occupation" in args:
+        for x in USERS:
+            if  args["occupation"].lower() in x["occupation"].lower():
+                output.append(x)
 
-    # Implement search here!
+    return output
 
-    return USERS
